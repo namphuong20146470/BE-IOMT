983552a020daa0dffc19fced7cf408aa0f180a97
namlho <121612584@dntu.edu.vn>
2025-09-26 09:07:01 +0700

    cors

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ const httpPort = process.env.HTTP_PORT || 3006;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Replace the existing test route with this HTML version that includes auto-redirect
 
